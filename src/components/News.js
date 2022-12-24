284384910e3ef6d5bb8f2392ed4e9404645f0427
Amankr705 <ak.1000aman@gmail.com>
2022-12-24 20:39:01 +0530

    Feat: spinner-loading

--- src/components/News.js
+++ src/components/News.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
+import Spinner from './Spinner';
 
 export class News extends Component {
 
@@ -13,37 +14,44 @@ export class News extends Component {
     }
 
     async componentDidMount(){
-        let url = 'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=51918305c7dc468fa49f0b88735440bf&page=1&pageSize=20';
+        let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=51918305c7dc468fa49f0b88735440bf&page=1&pageSize=${this.props.pageSize}`;
+        this.setState({loading: true});
         let data = await fetch(url);
         let parseData = await data.json();
         // console.log(parseData);
-        this.setState({articles: parseData.articles, totalResults: parseData.totalResults})
+        this.setState({articles: parseData.articles,     totalResults: parseData.totalResults,
+        loading: false
+        })
     };
 
   handlePrevClick = async () => {
-    let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=51918305c7dc468fa49f0b88735440bf&page=${this.state.page - 1}&pageSize=20`;
+    let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=51918305c7dc468fa49f0b88735440bf&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
+    this.setState({loading: true});
     let data = await fetch(url);
     let parseData = await data.json();
     // console.log(parseData);
     this.setState({
       page: this.state.page - 1,
-      articles: parseData.articles
+      articles: parseData.articles,
+      loading: false
     })
 
   };
   
   handleNextClick = async () => {
     // console.log('Next Page');
-    if(this.state.page + 1 > Math.ceil(this.state.totalResults/20)){
+    if(this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize)){
 
     }else{
-      let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=51918305c7dc468fa49f0b88735440bf&page=${this.state.page + 1}&pageSize=20`;
+      let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=51918305c7dc468fa49f0b88735440bf&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+      this.setState({loading: true});
       let data = await fetch(url);
       let parseData = await data.json();
       // console.log(parseData);
       this.setState({
         page: this.state.page + 1,
-        articles: parseData.articles
+        articles: parseData.articles,
+        loading: false
       })
     }
     
@@ -52,9 +60,10 @@ export class News extends Component {
   render() {
     return (
       <div className='container my-3'>
-        <h1>NewsBuzz - Top Headlines</h1>
+        <h1 className="text-center">NewsBuzz - Top Headlines</h1>
+        {this.state.loading && <Spinner/>}
         <div className="row">
-          {this.state.articles.map((element) => {
+          {!this.state.loading && this.state.articles.map((element) => {
             return <div className="col-md-4" key={element.url} >
                 <NewsItem title={element.title?element.title.slice(0, 45):""} description={element.description?element.description.slice(0, 89):''} imageUrl={element.urlToImage} newsUrl={element.url} />
             </div>
@@ -62,7 +71,7 @@ export class News extends Component {
         </div>
         <div className="container d-flex justify-content-between ">
           <button disabled={this.state.page<=1} type="button" className="btn btn-secondary" onClick={this.handlePrevClick} > &larr; Previous</button>
-          <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults/20)} type="button" className="btn btn-success" onClick={this.handleNextClick} >Next &rarr;</button>
+          <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize)} type="button" className="btn btn-success" onClick={this.handleNextClick} >Next &rarr;</button>
 
         </div>       
       </div>
